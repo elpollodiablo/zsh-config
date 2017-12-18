71391b29e9d6fb6e86f909b6bb5a1e5b73208790
Philip Poten <philip@poten.at>
2017-12-18 10:34:40 +0100

    add zsh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -3,6 +3,9 @@
 SCRIPT_DIR=`dirname $0`
 FULL_DIR="`( cd \"$SCRIPT_DIR\" && pwd )`"
 
+test -f /usr/local/bin/brew && brew install zsh
+test -f /usr/bin/apt-get && sudo apt-get install -y zsh zsh-doc
+
 rm -f ~/.zshrc
 ln -s $FULL_DIR/zshrc ~/.zshrc
 
