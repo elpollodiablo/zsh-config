60258ab7bafd6e9d25c9cf532094384c29fc8f78
Philip Poten <philip.poten@gmail.com>
2018-01-02 01:56:33 +0000

    fix test -e, add sudo to ln

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -7,9 +7,9 @@ test -f /usr/local/bin/brew && brew install zsh
 test -f /usr/bin/apt-get && sudo apt-get install -y zsh zsh-doc
 
 if [[ ! -f /usr/local/bin/zsh ]]; then
-	test -f /bin/zsh && ln -s /bin/zsh /usr/local/bin/zsh
-	test -f /usr/bin/zsh && ln -s /usr/bin/zsh /usr/local/bin/zsh
-	test -f /usr/local/bin/zsh || ( echo omg no zsh; exit 1 );
+	test -e /bin/zsh && sudo ln -s /bin/zsh /usr/local/bin/zsh
+	test -e /usr/bin/zsh && sudo ln -s /usr/bin/zsh /usr/local/bin/zsh
+	test -e /usr/local/bin/zsh || ( echo omg no zsh; exit 1 );
 fi
 rm -f ~/.zshrc
 ln -s $FULL_DIR/zshrc ~/.zshrc
